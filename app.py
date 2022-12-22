b1dc4e38442deb4eaade9dbfd4993726fbf8834e
Surawut Saithong-in <surawut.kong@gmail.com>
2022-12-23 00:50:49 +0700

    fix index and add homepage

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def home():
+def index():
     name = "Surawut"
     lastname = "Saithong-in"
-    return f"Hello My name is {name} {lastname}"
+    msg = f"Hello My name is {name} {lastname}"
+    return render_template("index.html")
+
 
 @app.route("/game")
 def game():
-    return render_template('index.html')
+    return render_template("game/index.html")
+
+
+@app.route("/home")
+def home():
+    name = request.args.get("name", "Surawut")
+    profile_name = f"name = {name}"
+    return render_template("home/index.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
